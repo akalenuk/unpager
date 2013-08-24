5fd3b881dc57e9f630d7290689682fce113cc07f
Alexandr Kalenuk <akalenuk@gmail.com>
2013-08-24 15:50:12 +0300

    translate some math from python

--- unpager/Triangulation.cs
+++ unpager/Triangulation.cs
@@ -20,50 +20,52 @@ using System.Diagnostics;
 
 namespace WindowsFormsApplication1 {
     class Triangulation {
+        static double sq(double x1, double y1, double x2, double y2, double x3, double y3) {
+            return Vector.cross(new double[2][] { new double[] { x2 - x1, y2 - y1, 0.0 }, new double[] { x3 - x1, y3 - y1, 0.0 } })[2];
+        }
+
+        static bool in_tri(double x, double y, int i, int[][] tris, double[][] xs) {
+            double x1 = xs[tris[i][0]][0];
+            double y1 = xs[tris[i][0]][1];
+            double x2 = xs[tris[i][1]][0];
+            double y2 = xs[tris[i][1]][1];
+            double x3 = xs[tris[i][2]][0];
+            double y3 = xs[tris[i][2]][1];
+            if (x < Math.Min(Math.Min(x1, x2), x3)) return false;
+            if (x > Math.Max(Math.Max(x1, x2), x3)) return false;
+            if (y < Math.Min(Math.Min(y1, y2), y3)) return false;
+            if (y > Math.Max(Math.Max(y1, y2), y3)) return false;
+            double S =sq(x1,y1, x2,y2, x3,y3);
+            double s1=sq(x,y, x2,y2, x3,y3);
+            double s2=sq(x1,y1, x,y, x3,y3);
+            double s3=sq(x1,y1, x2,y2, x,y);
+            if(Scalar.semi_equal((S - s1 - s2 - s3), 0.0)) return true;
+            return false;
+        }
+
+        static bool crosses(int i1, int i2, int j1, int j2, double[][] xs) { 
+            double ta = (xs[i2][0]-xs[i1][0])*(xs[j1][1]-xs[j2][1]) - (xs[j1][0]-xs[j2][0])*(xs[i2][1]-xs[i1][1]);
+            double tb = (xs[j1][0]-xs[j2][0])*(xs[i2][1]-xs[i1][1]) - (xs[i2][0]-xs[i1][0])*(xs[j1][1]-xs[j2][1]);
+            if (Scalar.semi_equal(ta, 0.0) || Scalar.semi_equal(tb, 0.0)) return false;
+            double a = ( (xs[j1][1]-xs[j2][1])*(xs[j1][0]-xs[i1][0]) - (xs[j1][0]-xs[j2][0])*(xs[j1][1]-xs[i1][1]) ) / ta;
+            double b = ( (xs[i2][1]-xs[i1][1])*(xs[j1][0]-xs[i1][0]) - (xs[i2][0]-xs[i1][0])*(xs[j1][1]-xs[i1][1]) ) / tb;
+            if (a < 0 || a > 1.0 || b < 0 || b > 1.0) return false;
+            return true;
+        }
+
+        static double d(int a, int b, double[][] xs){
+            return Math.Sqrt(Math.Pow(xs[a][0] - xs[b][0], 2) + Math.Pow(xs[a][1] - xs[b][1], 2));
+        }
+    
+
+        static double forma(int i1, int i2, int i3, double[][] xs) {
+            double the_forma = 0.0;
+            the_forma = Math.Abs(Math.Min(d(i1, i2, xs), d(i2, i3, xs)) / Math.Max(d(i1, i2, xs), d(i2, i3, xs)));
+            the_forma += Math.Abs(Math.Min(d(i2, i3, xs), d(i3, i1, xs)) / Math.Max(d(i2, i3, xs), d(i3, i1, xs)));
+            the_forma += Math.Abs(Math.Min(d(i3, i1, xs), d(i1, i2, xs)) / Math.Max(d(i3, i1, xs), d(i1, i2, xs)));
+            return the_forma;
+        }
         /*
-        def in_tri(x, y, i,  tris, xs):
-    x1=xs[tris[i][0]][0]
-    y1=xs[tris[i][0]][1]
-    x2=xs[tris[i][1]][0]
-    y2=xs[tris[i][1]][1]
-    x3=xs[tris[i][2]][0]
-    y3=xs[tris[i][2]][1]
-    if x<min(x1,x2,x3):
-        return False
-    if x>max(x1,x2,x3):
-        return False
-    if y<min(y1,y2,y3):
-        return False
-    if y>max(y1,y2,y3):
-        return False
-    S=sq(x1,y1, x2,y2, x3,y3);
-    s1=sq(x,y, x2,y2, x3,y3);
-    s2=sq(x1,y1, x,y, x3,y3);
-    s3=sq(x1,y1, x2,y2, x,y);
-    if abs(S-s1-s2-s3)<EPS:
-        return True
-    return False
-
-
-def crosses(i1,i2, j1,j2,  xs):
-    ta=(xs[i2][0]-xs[i1][0])*(xs[j1][1]-xs[j2][1]) - (xs[j1][0]-xs[j2][0])*(xs[i2][1]-xs[i1][1])
-    tb=(xs[j1][0]-xs[j2][0])*(xs[i2][1]-xs[i1][1]) - (xs[i2][0]-xs[i1][0])*(xs[j1][1]-xs[j2][1])
-    if ta==0 or tb==0:
-        return False
-    a=( (xs[j1][1]-xs[j2][1])*(xs[j1][0]-xs[i1][0]) - (xs[j1][0]-xs[j2][0])*(xs[j1][1]-xs[i1][1]) ) / ta
-    b=( (xs[i2][1]-xs[i1][1])*(xs[j1][0]-xs[i1][0]) - (xs[i2][0]-xs[i1][0])*(xs[j1][1]-xs[i1][1]) ) / tb
-    if 1>a>0 and 1>b>0:
-        return True;
-    return False;
-
-
-def forma(i1, i2, i3,  xs):
-    the_forma = abs( min(d(i1,i2, xs), d(i2,i3, xs)) / max(d(i1,i2, xs), d(i2,i3, xs)) )
-    the_forma+= abs( min(d(i2,i3, xs), d(i3,i1, xs)) / max(d(i2,i3, xs), d(i3,i1, xs)) )
-    the_forma+= abs( min(d(i3,i1, xs), d(i1,i2, xs)) / max(d(i3,i1, xs), d(i1,i2, xs)) )
-    return the_forma
-
-
 def triangulate(xs):
     tris=[[0,0,0]]
     icnt=0
