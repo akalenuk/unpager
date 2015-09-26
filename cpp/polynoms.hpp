dbe116b0a484a72d42dfb6016da1d6462faf1129
Olexandr Kaleniuk <akalenuk@gmail.com>
2015-09-26 18:35:44 +0300

    Polynoms constructor started

--- cpp/polynoms.hpp
+++ cpp/polynoms.hpp
@@ -77,4 +77,41 @@ namespace polynoms
         Data no_data;
         return approximate_and_interpolate<N>(input, no_data, solver, 0.0, output);
     }
+
+    namespace constructor{
+
+        template <int N>
+        std::array<double, N> p(double x){
+            std::array<double, N> ret;
+            for(int i = 0; i < N; i++){
+                ret[i] = std::pow(x, i);
+            }
+        }
+
+        template <int N>
+        std::array<double, N> dp(double x, int d){
+            std::array<double, N> ret;
+            for(int i = 0; i < N; i++){
+                if(i < d){
+                    ret[i] = 0.0;
+                }else{
+                    ret[i] = pow(x, i - d);
+                    for(int j = i; j > i-d; j--){
+                        ret[i] *= j;
+                    }
+                }
+            }
+        }
+
+        template <int N>
+        std::array<double, N> ip(double x1, double x2, int d){
+            std::array<double, N> ret;
+            for(int i = 0; i < N; i++){
+                ret[i] = pow(x2, i + d) - pow(x1, i + d);
+                for(int j = i; j < i+d; j++){
+                    ret[i] /= j;
+                }
+            }
+        }
+    }
 }
