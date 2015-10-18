809051fdebfc1d432b05bb683b4db13eff4e4ff3
Oleksandr Kaleniuk <akalenuk@gmail.com>
2015-10-18 10:36:16 +0300

    Fixed oneliner invert

--- python/oneliners.py
+++ python/oneliners.py
@@ -19,25 +19,27 @@ def mul(A, X):
 def project(A, Pn, Pd):
 	return add(A, scale(Pn, (Pd - dot(Pn, A)) / dot(Pn, Pn)))
 
-def error(A, B, X):
-	return sum([abs(di) for di in add(mul(A, X), scale(B, -1.0))])
+def distance(A, B):
+	return pow(dot( *(add(A, scale(B, -1.0)), )*2 ), 0.5)
 
 def solve(A, B, Xi):
-	return Xi if error(A, B, Xi) < 0.0001 else solve(A[1:] + [A[0]], B[1:] + [B[0]], project(Xi, A[0], B[0]))
+	return Xi if distance(mul(A, Xi), B) < 0.0001 else solve(A[1:] + [A[0]], B[1:] + [B[0]], project(Xi, A[0], B[0]))
 
 def invert(A):
-	return [solve(A, ort, [0.0]*len(A)) for ort in ident(len(A))]
+	return transpose([solve(A, ort, [0.0]*len(A)) for ort in ident(len(A))])
 
 if __name__ == "__main__":
 	A = [[0.78, -0.42, 0.59], [0.95, 0.31, 0.26], [-0.44, -0.1, -0.38]]
 	B = [1.0, 2.0, 3.0]
 	print 'A, B:', A, B
 	X = solve(A, B, [0.0]*3)
-	err = error(A, B, X)
+	err = distance(mul(A, X), B)
 	print 'X, error:', X, err
 	Ai = invert(A)
 	print 'A^-1:', Ai
 	Aii = invert(Ai)
 	print '(A^-1)^-1:',Aii
+	print B
 	X = mul(Ai, B)
+	err = distance(mul(A, X), B)
 	print 'Ai*B, error:', X, err
